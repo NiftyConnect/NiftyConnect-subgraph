2ad54ef263c584a97b47c702973bb7f77ce01cca
redefiine628 <redefiine628@gmail.com>
2022-05-03 11:32:04 +0800

    fix: change contract address

--- src/mapping.ts
+++ src/mapping.ts
@@ -24,52 +24,6 @@ export function handleOrderApprovedPartOne(event: OrderApprovedPartOne): void {
   entity.ipfsHash = params.ipfsHash.toHex();
 
   entity.save();
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.name(...)
-  // - contract.tokenTransferProxy(...)
-  // - contract.exchangeFeeRate(...)
-  // - contract.staticCall(...)
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.royaltyRegisterHub(...)
-  // - contract.takerRelayerFeeShare(...)
-  // - contract.version(...)
-  // - contract.protocolFeeRecipient(...)
-  // - contract.registry(...)
-  // - contract.nonces(...)
-  // - contract.cancelledOrFinalized(...)
-  // - contract.owner(...)
-  // - contract.protocolFeeShare(...)
-  // - contract._deriveDomainSeparator(...)
-  // - contract.INVERSE_BASIS_POINT(...)
-  // - contract.approvedOrders(...)
-  // - contract.makerRelayerFeeShare(...)
-  // - contract.merkleValidatorContract(...)
-  // - contract.splitToMerkleRootAndProof(...)
-  // - contract.buildCallData(...)
-  // - contract.guardedArrayReplace(...)
-  // - contract.calculateFinalPrice(...)
-  // - contract.hashToSign_(...)
-  // - contract.validateOrderParameters_(...)
-  // - contract.validateOrder_(...)
-  // - contract.calculateCurrentPrice_(...)
-  // - contract.ordersCanMatch_(...)
-  // - contract.orderCalldataCanMatch(...)
-  // - contract.calculateMatchPrice_(...)
 }
 
 export function handleOrderApprovedPartTwo(event: OrderApprovedPartTwo): void {
